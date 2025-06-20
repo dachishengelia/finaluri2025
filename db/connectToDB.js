a16b18c440c08b587da149e47f43576f4f7aa0f5
dachishengelia <shengeliaqeti111@gmail.com>
2025-06-20 17:35:02 +0400

    my changes

--- db/connectToDB.js
+++ db/connectToDB.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI);  
         console.log('Connected to MongoDB successfully');
     } catch (error) {
         console.error('Couldnt connect to mongoDB');
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/authRoutes')
 const postRoutes = require('./routes/postRoutes')
 const swaggerUi = require('swagger-ui-express')
 const swaggerJsdoc = require('swagger-jsdoc')
+const connectToDb = require('./path/to/connectToDB')  
 
 const app = express()
 
@@ -14,7 +14,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 const PORT = process.env.PORT || 3000
-const MONGO_URI = process.env.MONGO_URI
 
 
 const swaggerDefinition = {
@@ -26,20 +25,19 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:3000',
+      url: 'http://localhost:' + PORT,
       description: 'Local development server',
-    }
-  ]
+    },
+  ],
 }
 
 const options = {
   swaggerDefinition,
-  apis: ['./routes/*.js'], 
+  apis: ['./routes/*.js'],
 }
 
 const swaggerSpec = swaggerJsdoc(options)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-
 
 app.get('/', (req, res) => {
   res.send('Welcome! The server is up and running.')
@@ -49,15 +47,16 @@ app.use('/api/users', authRoutes)
 app.use('/api/posts', postRoutes)
 
 
-mongoose.connect(MONGO_URI)
+connectToDb()
   .then(() => {
     console.log('Connected to MongoDB successfully')
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`)
-      console.log(`Swagger on http://localhost:${PORT}/api-docs`)
+      console.log(`Swagger at http://localhost:${PORT}/api-docs`)
     })
   })
-  .catch(err => {
-    console.error('Failed to connect to mongoDB')
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB')
     console.error(err)
+    process.exit(1)  
   })
